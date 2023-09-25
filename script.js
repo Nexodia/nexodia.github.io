a0479e8b8c3409ad0e6f092f9ccbc9f58f941ff7
Nesito <85034444+Nexodia@users.noreply.github.com>
2023-09-24 22:08:36 -0300

    Pong 2.0

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ class Bolita {
 }
 
 class Rectangulo {
-    constructor(color, x, y, w = 30, h = 140) {
+    constructor(color, x, y, w = 20, h = 100) {
         this.x = x;
         this.y = y;
         this.w = w;
@@ -34,15 +34,18 @@ class Rectangulo {
 let circular = new Bolita("white");
 let rectizq = new Rectangulo( "#03a9f4", canvas.width/12, canvas.height/2)
 let rectder = new Rectangulo("cyan", canvas.width*11/12, canvas.height/2)
-let xmov = 5
-let ymov = 5
-let vel = 5
+let xmov = 3
+let ymov = 3
+let velx = 5
+let vely = 5
 let lado = 1
 let busy = false
 let puntos = 0
 let best = 0
 let busyP = false
-
+let rectvel = 8
+let handicap = true
+let hz = 60
 function pointHandler() {
     if (!busyP) {
         busyP = true
@@ -61,108 +64,152 @@ document.addEventListener( "keyup", (event) => {
     keys[event.key] = false;
 })
 
+let sesenta = document.getElementById("sesenta")
+let cien = document.getElementById("cien")
 
-setInterval(() => {
-    ctx.clearRect(0,0,900,600);
+sesenta.addEventListener("click", function() {
+    hz = 60
+    console.log(hz)
+})
+cien.addEventListener("click", function() {
+    hz = 144
+    console.log(hz)
+})
 
-    if (keys['w'] && rectizq.y >= 0 + rectizq.gethh()) {
-        rectizq.y -= 4;
-    }
-    if (keys['s'] && rectizq.y <= canvas.height - rectizq.gethh()) {
-        rectizq.y += 4;
-    }
-    if (keys['i'] && rectder.y >= 0 + rectder.gethh()) {
-        rectder.y -= 4;
-    }
-    if (keys['k'] && rectder.y <= canvas.height - rectder.gethh()) {
-        rectder.y += 4;
-    }
-    if (keys['r']) {
-        if (!busy) {    
-            circular.x = 450
-            circular.y = 300
-            xmov = 5 * ladorand()
-            ymov = 5 * ladorand()
-            rectder.y = 300
-            rectizq.y = 300
-            lado *= -1
-            busy = true
-            puntos = 0
-            setTimeout(()=> {
-                busy = false
-            }, 750)
+
+let play = false
+let pongbox = document.getElementById("pongbox")
+let pongtitle = document.getElementById("pongtitle")
+
+if (!play) {pongbox.addEventListener("click", function() { 
+    canvas.classList.remove("ponghover")
+    pongtitle.textContent = ""
+    
+    console.log(canvas.classList)
+    setInterval(() => {
+        ctx.clearRect(0,0,900,600);
+
+        if (keys['w'] && rectizq.y >= 0 + rectizq.gethh()) {
+            rectizq.y -= rectvel
         }
-    }
-    
-    circular.x += xmov;
-    circular.y += ymov;
-    
-    if (circular.x >= 900 - circular.radio) {
-        //Red wins
-        xmov = 0
-        ymov = 0
-    } 
-    
-    if (circular.x <= 0 + circular.radio) {
-        //Blu wins
-        xmov = 0
-        ymov = 0
-    }
-    
-    if (circular.y >= 600 -circular.radio) {
-        ymov = -vel
-    }
+        if (keys['s'] && rectizq.y <= canvas.height - rectizq.gethh()) {
+            rectizq.y += rectvel
+        }
+        if (keys['i'] && rectder.y >= 0 + rectder.gethh()) {
+            rectder.y -= rectvel
+        }
+        if (keys['k'] && rectder.y <= canvas.height - rectder.gethh()) {
+            rectder.y += rectvel
+        }
+        if (keys['r']) {
+            if (!busy) {    
+                circular.x = 450
+                circular.y = 300 * Math.random()
+                xmov = 3 * ladorand()
+                ymov = 3 * ladorand()
+                velx = 5
+                vely = 5
+                rectder.y = 300
+                rectizq.y = 300
+                lado *= -1
+                busy = true
+                puntos = 0
+                handicap = true
+                setTimeout(()=> {
+                    busy = false
+                }, 750)
+            }
+        }
+        
+        circular.x += xmov;
+        circular.y += ymov;
+        
+        if (circular.x >= 900 - circular.radio) {
+            //Red wins
+            xmov = 0
+            ymov = 0
+        } 
+        
+        if (circular.x <= 0 + circular.radio) {
+            //Blu wins
+            xmov = 0
+            ymov = 0
+        }
+        
 
-    if (circular.y <= 0 + circular.radio) {
-        ymov = vel
-    }
-    
-    
-    if (circular.x >= rectizq.x - rectizq.w/2 - circular.radio && circular.x <= rectizq.x + rectizq.w/2 + circular.radio && circular.y >= rectizq.y - rectizq.h/2 - circular.radio && circular.y <= rectizq.y - rectizq.h/2 - circular.radio + 5){
-        ymov = -vel
-        pointHandler()
-    } else if (circular.x >= rectizq.x - rectizq.w/2 - circular.radio && circular.x <= rectizq.x + rectizq.w/2 + circular.radio && circular.y >= rectizq.y + rectizq.h/2 + circular.radio -5 && circular.y <= rectizq.y + rectizq.h/2 + circular.radio){
-        ymov = vel
-        pointHandler()
-    } else if (circular.x >= rectizq.x - rectizq.w/2 - circular.radio && circular.x <= rectizq.x + rectizq.w/2 + circular.radio && circular.y >= rectizq.y - rectizq.h/2 - circular.radio && circular.y <= rectizq.y + rectizq.h/2 + circular.radio) {
-        xmov = vel
-        pointHandler()
-    }
-    
-    if (circular.x >= rectder.x - rectder.w/2 - circular.radio && circular.x <= rectder.x + rectder.w/2 + circular.radio && circular.y >= rectder.y - rectder.h/2 - circular.radio && circular.y <= rectder.y - rectder.h/2 - circular.radio + 5){
-        ymov = -vel
-        pointHandler()
-    } else if (circular.x >= rectder.x - rectder.w/2 - circular.radio && circular.x <= rectder.x + rectder.w/2 + circular.radio && circular.y >= rectder.y + rectder.h/2 + circular.radio -5 && circular.y <= rectder.y + rectder.h/2 + circular.radio){
-        ymov = vel
-        pointHandler()
-    } else if (circular.x >= rectder.x - rectder.w/2 - circular.radio && circular.x <= rectder.x + rectder.w/2 + circular.radio && circular.y >= rectder.y - rectder.h/2 - circular.radio && circular.y <= rectder.y + rectder.h/2 + circular.radio) {
-        xmov = -vel
-        pointHandler()
-    }
-    
-    best = Math.max(puntos, best)
+        if (circular.y >= 600 -circular.radio) {
+            if (handicap) {ymov = -3} 
+            else {ymov = -Math.abs(vely) }
+        }
 
-    ctx.font = "150px Ticketing";
-    ctx.fillStyle = "#ffffff77";
-    ctx.textAlign = "center";
-    ctx.fillText(puntos, canvas.width/2, canvas.height/2 + 50);
+        if (circular.y <= 0 + circular.radio) {
+            if (handicap) {ymov = 3}
+            else {ymov = Math.abs(vely)}
+        }
+        
+        
+        if (circular.x >= rectizq.x - rectizq.w/2 - circular.radio &&
+            circular.x <= rectizq.x + rectizq.w/2 + circular.radio &&
+            circular.y >= rectizq.y - rectizq.h/2 - circular.radio &&
+            circular.y <= rectizq.y - rectizq.h/2 - circular.radio + 5){
+            ymov = -vely
+            pointHandler()
 
-    ctx.font = "50px Ticketing";
-    ctx.fillStyle = "#ffffff33";
-    ctx.textAlign = "center";
-    ctx.fillText(best, canvas.width/2, canvas.height/2 + 150);
-    
-    ctx.beginPath();
-    ctx.arc(circular.x, circular.y, circular.radio, 0, 2 * Math.PI);
-    ctx.fillStyle = circular.color;
-    ctx.fill();
-    
-    ctx.fillStyle = rectizq.color
-    ctx.fillRect(rectizq.x - rectizq.w/2, rectizq.y - rectizq.h/2, rectizq.w, rectizq.h)
-    ctx.fillStyle = rectder.color
-    ctx.fillRect(rectder.x- rectder.w/2, rectder.y - rectder.h/2, rectder.w, rectder.h)
+        } else if (circular.x >= rectizq.x - rectizq.w/2 - circular.radio &&
+                circular.x <= rectizq.x + rectizq.w/2 + circular.radio && 
+                circular.y >= rectizq.y + rectizq.h/2 + circular.radio -5 && 
+                circular.y <= rectizq.y + rectizq.h/2 + circular.radio){
+            ymov = vely
+            pointHandler()
+        } else if (circular.x >= rectizq.x - rectizq.w/2 - circular.radio && 
+                circular.x <= rectizq.x + rectizq.w/2 + circular.radio && 
+                circular.y >= rectizq.y - rectizq.h/2 - circular.radio && 
+                circular.y <= rectizq.y + rectizq.h/2 + circular.radio) {
+            xmov = velx
+            ymov += computeYvel(circular.y - rectizq.y, rectizq.h)
+            vely = ymov
+            handicap = false
+            pointHandler()
+        }
+        
+        if (circular.x >= rectder.x - rectder.w/2 - circular.radio && circular.x <= rectder.x + rectder.w/2 + circular.radio && circular.y >= rectder.y - rectder.h/2 - circular.radio && circular.y <= rectder.y - rectder.h/2 - circular.radio + 5){
+            ymov = -vely
+            pointHandler()
+        } else if (circular.x >= rectder.x - rectder.w/2 - circular.radio && circular.x <= rectder.x + rectder.w/2 + circular.radio && circular.y >= rectder.y + rectder.h/2 + circular.radio -5 && circular.y <= rectder.y + rectder.h/2 + circular.radio){
+            ymov = vely
+            pointHandler()
+        } else if (circular.x >= rectder.x - rectder.w/2 - circular.radio && circular.x <= rectder.x + rectder.w/2 + circular.radio && circular.y >= rectder.y - rectder.h/2 - circular.radio && circular.y <= rectder.y + rectder.h/2 + circular.radio) {
+            xmov = -velx
+            ymov += computeYvel(circular.y - rectder.y, rectder.h)
+            vely = ymov
+            handicap = false
+            pointHandler()
+        }
+        
+        best = Math.max(puntos, best)
 
-}, 1000/144)
+        ctx.font = "150px Ticketing";
+        ctx.fillStyle = "#ffffff77";
+        ctx.textAlign = "center";
+        ctx.fillText(puntos, canvas.width/2, canvas.height/2 + 50);
+
+        ctx.font = "50px Ticketing";
+        ctx.fillStyle = "#ffffff33";
+        ctx.textAlign = "center";
+        ctx.fillText(best, canvas.width/2, canvas.height/2 + 150);
+        
+        ctx.beginPath();
+        ctx.arc(circular.x, circular.y, circular.radio, 0, 2 * Math.PI);
+        ctx.fillStyle = circular.color;
+        ctx.fill();
+        
+        ctx.fillStyle = rectizq.color
+        ctx.fillRect(rectizq.x - rectizq.w/2, rectizq.y - rectizq.h/2, rectizq.w, rectizq.h)
+        ctx.fillStyle = rectder.color
+        ctx.fillRect(rectder.x- rectder.w/2, rectder.y - rectder.h/2, rectder.w, rectder.h)
+
+    }, 1000/hz)
+})}
 
 document.fonts.ready.then(() => draw());
 
@@ -179,4 +226,8 @@ function ladorand() {
     let guess = randint(1,2)
     if (guess == 2){return 1}
     else {return -1}
+}
+
+function computeYvel(distancia, altura) {
+    return distancia/((altura/4)-2)
 }
